15b85e3f55af8a4f8a3291463378886326f5031d
Kwame <kwame.mintah@cgi.com>
2018-10-09 15:32:42 +0000

    Change and removing methods & adding test specs

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,23 +1,29 @@
-time = Time.now.getlocal
-notes = []
 
-def Notes
+Class Notes
+  def initialize
+   @note = []
+  end
+ 
   def add(title, message) 
-   @notes[:"#{title}"] = message
+   @note = Note.new(title, message)
+  end
+  
+  def menu
+   time = Time.now.getlocal
+   puts "What is the title of your note?"
+   title = gets.chomp.capitalize
+   puts "Type your message below for '#{title}'"
+   message = gets.chomp
+   puts "#{time} - Note has been saved!"
+   add(title, message)
+  end
+  
+  def list
+   puts @note
   end
   
   def delete
       
   end
 
-  def list
-   puts @notes
-  end
-end
-
-puts "What is the title of your note?"
-title = gets.chomp.capitalize
-puts "Type your message below for '#{title}'"
-message = gets
-
-puts "#{time} - Note has been saved!"
+# Promptmenu
--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -3,8 +3,13 @@ require 'notes'
 
 describe Notes do 
   describe 'responds to adding notes' do 
-      
+    expect(subject).to respond_to(:add)
   end
+  describe 'menu' do
+   it 'respond to menu' do
+    expect(subject).to respond_to(:menu)
+  end
+ end
   it 'responds to list all notes' do
     expect(subject).to respond_to(:list)
    end
